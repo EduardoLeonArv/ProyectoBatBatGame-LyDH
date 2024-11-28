64622d86bd35fceac7dae7a050d5c5b81282266a
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-11-28 08:48:27 +0000

    Cambios en duplications

--- src/main/java/al/tonikolaba/gamestate/Level3State.java
+++ src/main/java/al/tonikolaba/gamestate/Level3State.java
@@ -1,7 +1,3 @@
-/** Copyright to N.Kolaba
- All rights reserved ©.
- **/
-
 package al.tonikolaba.gamestate;
 
 import al.tonikolaba.entity.Enemy.EnemyType;
@@ -11,31 +7,14 @@ import al.tonikolaba.tilemap.Background;
 /**
  * Clase que representa el nivel 3 del juego.
  * Gestiona los enemigos, eventos y el estado del nivel.
- *
- * @author N.Kolaba
  */
 public class Level3State extends GameState {
 
-    private boolean eventQuake;
-    private Player player; // Referencia al jugador
+	public Level3State(GameStateManager gsm, Player player) {
+		super(gsm, player);
+		init(GameStateManager.LEVEL4STATE);
+	}
 
-    /**
-     * Constructor del estado del nivel 3.
-     *
-     * @param gsm    Gestor de estados del juego.
-     * @param player Referencia al jugador.
-     */
-    public Level3State(GameStateManager gsm, Player player) {
-        super(gsm, player);
-        this.player = player;
-        init(GameStateManager.LEVEL4STATE);
-    }
-
-	/**
-	 * Inicializa los elementos del nivel 3.
-	 *
-	 * @param nextLevel Estado del siguiente nivel.
-	 */
 	@Override
 	public void init(int nextLevel) {
 		super.init(nextLevel);
@@ -50,63 +29,9 @@ public class Level3State extends GameState {
 		);
 	}
 
-	/**
-	 * Configura los enemigos del nivel.
-	 */
-	private void configureEnemies() {
-		enemyTypesInLevel = new EnemyType[] {
-				EnemyType.XHELBAT, EnemyType.ZOGU, EnemyType.UFO
-		};
-
-		coords = new int[][] {
-				{ 750, 100 }, { 900, 150 }, { 1320, 250 }, { 1570, 160 }, { 1590, 160 },
-				{ 2600, 370 }, { 2620, 370 }, { 2640, 370 }, { 904, 130 }, { 1080, 270 },
-				{ 1200, 270 }, { 1704, 300 }, { 1900, 580 }, { 2330, 550 }, { 2400, 490 },
-				{ 2457, 430 }
-		};
-
-		populateEnemies(enemyTypesInLevel, coords);
-	}
-
-	/**
-	 * Actualiza el estado del nivel.
-	 */
 	@Override
 	public void update() {
 		super.update();
 		handleEventQuake(2175, new int[]{60, 120, 150, 180, 300});
-	}
-
-	/**
-	 * Gestiona el evento de terremoto.
-	 */
-	private void eventQuake() {
-		eventCount++;
-		switch (eventCount) {
-			case 1:
-				player.stop();
-				player.setPosition(2175, player.gety());
-				break;
-			case 60:
-				player.setEmote(Player.CONFUSED_EMOTE);
-				break;
-			case 120:
-				player.setEmote(Player.NONE_EMOTE);
-				break;
-			case 150:
-				tileMap.setShaking(true, 10);
-				break;
-			case 180:
-				player.setEmote(Player.SURPRISED_EMOTE);
-				break;
-			case 300:
-				player.setEmote(Player.NONE_EMOTE);
-				eventQuake = blockInput = false;
-				eventCount = 0;
-				break;
-			default:
-				// No action for other counts
-				break;
-		}
 	}
 }
--- src/main/java/al/tonikolaba/gamestate/LevelTest.java
+++ src/main/java/al/tonikolaba/gamestate/LevelTest.java
@@ -1,39 +1,20 @@
+package al.tonikolaba.gamestate;
+
+import al.tonikolaba.entity.Enemy.EnemyType;
+import al.tonikolaba.entity.Player;
+import al.tonikolaba.tilemap.Background;
+
 /**
  * Clase que representa el nivel de prueba en el juego.
  * Gestiona enemigos, eventos y el estado del nivel.
- *
- * @author N.Kolaba
  */
-package al.tonikolaba.gamestate;
-
-import al.tonikolaba.entity.Player;
-import al.tonikolaba.entity.Enemy.EnemyType;
-import al.tonikolaba.tilemap.Background;
-
 public class LevelTest extends GameState {
 
-    private static final String LEVEL_BOSS_MUSIC_NAME = "level1boss";
-    private static final int[][] ENEMY_COORDINATES = {{150, 100}};
-    private static final EnemyType[] ENEMY_TYPES = {EnemyType.SPIRIT};
-
-    private boolean eventQuake;
-
-    /**
-     * Constructor del estado de nivel de prueba.
-     *
-     * @param gsm    Gestor de estados del juego.
-     * @param player Referencia al jugador.
-     */
     public LevelTest(GameStateManager gsm, Player player) {
         super(gsm, player);
         init(GameStateManager.ACIDSTATE);
     }
 
-    /**
-     * Inicializa los elementos del nivel.
-     *
-     * @param nextLevel Estado del siguiente nivel.
-     */
     @Override
     public void init(int nextLevel) {
         super.init(nextLevel);
@@ -48,78 +29,9 @@ public class LevelTest extends GameState {
         );
     }
 
-    /**
-     * Configura los enemigos del nivel.
-     */
-    private void configureEnemies() {
-        populateEnemies(ENEMY_TYPES, ENEMY_COORDINATES);
-    }
-
-    /**
-     * Actualiza el estado del nivel.
-     */
     @Override
     public void update() {
         super.update();
         handleEventQuake(100, new int[]{60, 120, 150, 180, 300});
-    }
-
-    /**
-     * Gestiona el evento de terremoto.
-     */
-    private void eventQuake() {
-        eventCount++;
-        switch (eventCount) {
-            case 1:
-                player.stop();
-                player.setPosition(120, player.gety());
-                setPlayerEmote(Player.CONFUSED_EMOTE, 0);
-                tileMap.setShaking(true, 10);
-                break;
-            case 60:
-                setPlayerEmote(Player.CONFUSED_EMOTE, 0);
-                break;
-            case 120:
-                setPlayerEmote(Player.NONE_EMOTE, 0);
-                break;
-            case 150:
-                tileMap.setShaking(true, 10);
-                break;
-            case 180:
-                setPlayerEmote(Player.SURPRISED_EMOTE, 0);
-                break;
-            case 300:
-                setPlayerEmote(Player.NONE_EMOTE, 0);
-                eventQuake = blockInput = false;
-                eventCount = 0;
-                break;
-            default:
-                break;
-        }
-    }
-
-    /**
-     * Configura el emote del jugador y ajusta el contador del evento si es necesario.
-     *
-     * @param emote Emote a asignar al jugador.
-     * @param delay Tiempo adicional para el evento.
-     */
-    private void setPlayerEmote(int emote, int delay) {
-        if (isValidEmote(emote)) {
-            player.setEmote(emote);
-            if (delay > 0) {
-                eventCount += delay;
-            }
-        }
-    }
-
-    /**
-     * Valida si el emote es válido para el jugador.
-     *
-     * @param emote Emote a validar.
-     * @return true si es válido, false en caso contrario.
-     */
-    private boolean isValidEmote(int emote) {
-        return emote == Player.CONFUSED_EMOTE || emote == Player.NONE_EMOTE || emote == Player.SURPRISED_EMOTE;
     }
 }
