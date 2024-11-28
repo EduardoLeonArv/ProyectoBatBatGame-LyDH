b08f64a87fbb59368b0b8a9164e28e7b3890e214
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-11-28 01:50:00 +0000

    Update Level4State.java

--- src/main/java/al/tonikolaba/gamestate/Level4State.java
+++ src/main/java/al/tonikolaba/gamestate/Level4State.java
@@ -3,113 +3,88 @@ package al.tonikolaba.gamestate;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.security.SecureRandom;
 
 import al.tonikolaba.audio.JukeBox;
-import al.tonikolaba.entity.Enemy;
-import al.tonikolaba.entity.EnergyParticle;
-import al.tonikolaba.entity.Explosion;
-import al.tonikolaba.entity.HUD;
-import al.tonikolaba.entity.Player;
-import al.tonikolaba.entity.PlayerSave;
-import al.tonikolaba.entity.Portal;
-import al.tonikolaba.entity.Spirit;
-import al.tonikolaba.entity.Teleport;
+import al.tonikolaba.entity.*;
 import al.tonikolaba.entity.batbat.Piece;
 import al.tonikolaba.entity.enemies.RedEnergy;
 import al.tonikolaba.handlers.Keys;
 import al.tonikolaba.main.GamePanel;
 import al.tonikolaba.tilemap.Background;
-import java.security.SecureRandom;
 
 /**
  * @author N.Kolaba
  */
-
 public class Level4State extends GameState {
 
 	private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 	private static final String LEVEL_BOSS_MUSIC_NAME = "level1boss";
 	private ArrayList<EnergyParticle> energyParticles;
-	private Piece tlp;
-	private Piece trp;
-	private Piece blp;
-	private Piece brp;
-
+	private Piece tlp, trp, blp, brp;
 	private Spirit spirit;
 
 	// events
 	private boolean blockInput = false;
 	private int eventCount = 0;
-	private boolean eventStart;
-	private ArrayList<Rectangle> tb;
-	private boolean eventFinish;
-	private boolean eventDead;
-	private boolean eventPortal;
-	private boolean flash;
-	private boolean eventBossDead;
+	private boolean eventStart, eventFinish, eventDead, eventPortal, flash, eventBossDead;
 	protected Portal portal;
+	private ArrayList<Rectangle> tb;
 
 	public Level4State(GameStateManager gsm, Player player) {
-		super(gsm, player); // Pasar el jugador al constructor padre
+		super(gsm, player);
 		init(GameStateManager.ACIDSTATE);
 	}
 
 	@Override
 	public void init(int nextLevel) {
-
 		super.init(nextLevel);
-		// backgrounds
-		temple = new Background("/Backgrounds/temple1.gif", 0.5, 0);
 
-		// tilemap
+		// Backgrounds and Tilemap
+		temple = new Background("/Backgrounds/temple1.gif", 0.5, 0);
 		generateTileMap("/Maps/level4.map", 0, 140, false);
 
+		// Setup entities and player
 		setupGameObjects(50, 190, 160, 154, true);
 		setupMusic(LEVEL_BOSS_MUSIC_NAME, "/Music/level1boss.mp3", true);
 
-		// player
 		player = new Player(tileMap);
 		player.setPosition(50, 190);
 		player.setHealth(PlayerSave.getHealth());
 		player.setLives(PlayerSave.getLives());
 		player.setTime(PlayerSave.getTime());
 
-		// explosions
-		explosions = new ArrayList<Explosion>();
-
-		// enemies
-		enemies = new ArrayList<Enemy>();
+		explosions = new ArrayList<>();
+		enemies = new ArrayList<>();
 		populateEnemies();
 
-		// energy particle
-		energyParticles = new ArrayList<EnergyParticle>();
-
-		// init player
+		energyParticles = new ArrayList<>();
 		player.init(enemies, energyParticles);
 
-		// hud
 		hud = new HUD(player);
 
-		// portal
 		portal = new Portal(tileMap);
 		portal.setPosition(270, 395);
 		teleport = new Teleport(tileMap);
 		teleport.setPosition(480, 40);
 
-		tlp = new Piece(tileMap, new int[] { 0, 0, 10, 10 });
-		trp = new Piece(tileMap, new int[] { 10, 0, 10, 10 });
-		blp = new Piece(tileMap, new int[] { 0, 10, 10, 10 });
-		brp = new Piece(tileMap, new int[] { 10, 10, 10, 10 });
+		initPieces();
+
+		eventStart = blockInput = true;
+		tb = new ArrayList<>();
+		eventStart();
+	}
+
+	private void initPieces() {
+		tlp = new Piece(tileMap, new int[]{0, 0, 10, 10});
+		trp = new Piece(tileMap, new int[]{10, 0, 10, 10});
+		blp = new Piece(tileMap, new int[]{0, 10, 10, 10});
+		brp = new Piece(tileMap, new int[]{10, 10, 10, 10});
 
 		tlp.setPosition(260, 345);
 		trp.setPosition(270, 345);
 		blp.setPosition(260, 355);
 		brp.setPosition(270, 355);
-
-		// start event
-		eventStart = blockInput = true;
-		tb = new ArrayList<Rectangle>();
-		eventStart();
 	}
 
 	private void populateEnemies() {
@@ -119,68 +94,70 @@ public class Level4State extends GameState {
 		enemies.add(spirit);
 	}
 
-	@Override
-	public void update() {
-		// check keys
-		handleInput();
+	private void updateEventRectangles(Rectangle rect, int deltaX, int deltaY, int deltaWidth, int deltaHeight) {
+		rect.x += deltaX;
+		rect.y += deltaY;
+		rect.width += deltaWidth;
+		rect.height += deltaHeight;
+	}
 
-		// check if boss dead event should start
-		if (!eventFinish && spirit.isDead()) {
-			eventBossDead = blockInput = true;
+	private void manageEvent(Rectangle rect, int maxCount, Runnable onComplete) {
+		eventCount++;
+		if (eventCount == 1) {
+			tb.clear();
+			tb.add(new Rectangle(GamePanel.WIDTH / 2, GamePanel.HEIGHT / 2, 0, 0));
+		} else if (eventCount <= maxCount) {
+			updateEventRectangles(tb.get(0), -6, -4, 12, 8);
 		}
-
-		// check if player dead event should start
-		if (player.getHealth() == 0 || player.gety() > tileMap.getHeight()) {
-			eventDead = blockInput = true;
+		if (eventCount == maxCount) {
+			onComplete.run();
+			eventCount = 0;
 		}
+	}
 
-		// play events
-		if (eventStart)
-			eventStart();
-		if (eventDead)
-			eventDead();
-		if (eventFinish)
-			eventFinish();
-		if (eventPortal)
-			eventPortal();
-		if (eventBossDead)
-			eventBossDead();
-
-		// move backgrounds
-		temple.setPosition(tileMap.getx(), tileMap.gety());
-
-		// update player
-		player.update();
-
-		// update tilemap
-		tileMap.setPosition((double) GamePanel.WIDTH - player.getx(),
-				(double) GamePanel.HEIGHT - player.gety());
-
-
-		// update enemies
-		for (int i = 0; i < enemies.size(); i++) {
-			Enemy e = enemies.get(i);
-			e.update();
-			if (e.isDead() || e.shouldRemove()) {
-				enemies.remove(i);
-				i--;
-				explosions.add(new Explosion(tileMap, e.getx(), e.gety()));
-			}
-		}
-
-		// update explosions
+	private void updateExplosions() {
 		for (int i = 0; i < explosions.size(); i++) {
-			explosions.get(i).update();
-			if (explosions.get(i).shouldRemove()) {
+			Explosion explosion = explosions.get(i);
+			explosion.update();
+			if (explosion.shouldRemove()) {
 				explosions.remove(i);
 				i--;
 			}
 		}
+	}
 
-		// update portal
+	private void updateEnemies() {
+		for (int i = 0; i < enemies.size(); i++) {
+			Enemy enemy = enemies.get(i);
+			enemy.update();
+			if (enemy.isDead() || enemy.shouldRemove()) {
+				enemies.remove(i);
+				i--;
+				explosions.add(new Explosion(tileMap, enemy.getx(), enemy.gety()));
+			}
+		}
+	}
+
+	@Override
+	public void update() {
+		handleInput();
+
+		if (!eventFinish && spirit.isDead()) eventBossDead = blockInput = true;
+		if (player.getHealth() == 0 || player.gety() > tileMap.getHeight()) eventDead = blockInput = true;
+
+		if (eventStart) eventStart();
+		if (eventDead) manageEvent(tb.get(0), 120, this::reset);
+		if (eventFinish) manageEvent(tb.get(0), 60, () -> gsm.setState(GameStateManager.ACIDSTATE));
+		if (eventPortal) eventPortal();
+		if (eventBossDead) eventBossDead();
+
+		temple.setPosition(tileMap.getx(), tileMap.gety());
+		player.update();
+		tileMap.setPosition((double) GamePanel.WIDTH - player.getx(), (double) GamePanel.HEIGHT - player.gety());
+		updateEnemies();
+		updateExplosions();
+
 		portal.update();
-
-		// update artfact
 		tlp.update();
 		trp.update();
 		blp.update();
@@ -189,44 +166,24 @@ public class Level4State extends GameState {
 
 	@Override
 	public void draw(Graphics2D g) {
-		// draw background
 		temple.draw(g);
-
-		// draw tilemap
 		tileMap.draw(g);
-
-		// draw portal
 		portal.draw(g);
 
-		// draw enemies
-		for (int i = 0; i < enemies.size(); i++) {
-			enemies.get(i).draw(g);
-		}
+		for (Enemy enemy : enemies) enemy.draw(g);
+		for (Explosion explosion : explosions) explosion.draw(g);
 
-		// draw explosions
-		for (int i = 0; i < explosions.size(); i++) {
-			explosions.get(i).draw(g);
-		}
-
-		// draw artifact
 		tlp.draw(g);
 		trp.draw(g);
 		blp.draw(g);
 		brp.draw(g);
 
-		// draw player
 		player.draw(g);
-
-		// draw hud
 		hud.draw(g);
 
-		// draw transition boxes
 		g.setColor(java.awt.Color.YELLOW);
-		for (int i = 0; i < tb.size(); i++) {
-			g.fill(tb.get(i));
-		}
+		for (Rectangle rect : tb) g.fill(rect);
 
-		// flash
 		if (flash) {
 			g.setColor(java.awt.Color.WHITE);
 			g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
@@ -235,27 +192,19 @@ public class Level4State extends GameState {
 
 	@Override
 	public void handleInput() {
-		if (Keys.isPressed(Keys.ESCAPE))
-			gsm.setPaused(true);
-		if (blockInput || player.getHealth() == 0)
-			return;
+		if (Keys.isPressed(Keys.ESCAPE)) gsm.setPaused(true);
+		if (blockInput || player.getHealth() == 0) return;
+
 		player.setUp(Keys.getKeyState()[Keys.UP]);
 		player.setLeft(Keys.getKeyState()[Keys.LEFT]);
 		player.setDown(Keys.getKeyState()[Keys.DOWN]);
 		player.setRight(Keys.getKeyState()[Keys.RIGHT]);
 		player.setJumping(Keys.getKeyState()[Keys.BUTTON1]);
 		player.setDashing(Keys.getKeyState()[Keys.BUTTON2]);
-		if (Keys.isPressed(Keys.BUTTON3))
-			player.setAttacking();
-		if (Keys.isPressed(Keys.BUTTON4))
-			player.setCharging();
+		if (Keys.isPressed(Keys.BUTTON3)) player.setAttacking();
+		if (Keys.isPressed(Keys.BUTTON4)) player.setCharging();
 	}
 
-///////////////////////////////////////////////////////
-//////////////////// EVENTS
-///////////////////////////////////////////////////////
-
-	// reset level
 	@Override
 	protected void reset() {
 		player.reset();
@@ -325,28 +274,6 @@ public class Level4State extends GameState {
 				reset();
 			}
 		}
-	}
-
-	// finished level
-	private void eventFinish() {
-		eventCount++;
-		if (eventCount == 1) {
-			tb.clear();
-			tb.add(new Rectangle(GamePanel.WIDTH / 2,
-					GamePanel.HEIGHT / 2, 0, 0));
-		} else if (eventCount > 1) {
-			tb.get(0).x -= 6;
-			tb.get(0).y -= 4;
-			tb.get(0).width += 12;
-			tb.get(0).height += 8;
-		}
-		if (eventCount == 60) {
-			PlayerSave.setHealth(player.getHealth());
-			PlayerSave.setLives(player.getLives());
-			PlayerSave.setTime(player.getTime());
-			gsm.setState(GameStateManager.ACIDSTATE);
-		}
-
 	}
 
 	private void eventPortal() {
