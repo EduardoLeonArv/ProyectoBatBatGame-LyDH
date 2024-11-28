31312514e6e480d83995d0bbb1d6cb8812b134dd
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-11-28 00:46:25 +0000

    Corrección de  Duplications

--- src/main/java/al/tonikolaba/gamestate/LevelTest.java
+++ src/main/java/al/tonikolaba/gamestate/LevelTest.java
@@ -1,4 +1,7 @@
 /**
+ * Clase que representa el nivel de prueba en el juego.
+ * Gestiona enemigos, eventos y el estado del nivel.
+ *
  * @author N.Kolaba
  */
 package al.tonikolaba.gamestate;
@@ -12,62 +15,100 @@ public class LevelTest extends GameState {
     private static final String LEVEL_BOSS_MUSIC_NAME = "level1boss";
     private boolean eventQuake;
 
+    /**
+     * Constructor del estado de nivel de prueba.
+     *
+     * @param gsm    Gestor de estados del juego.
+     * @param player Referencia al jugador.
+     */
     public LevelTest(GameStateManager gsm, Player player) {
         super(gsm, player);
         init(GameStateManager.ACIDSTATE);
     }
 
+    /**
+     * Inicializa los elementos del nivel.
+     *
+     * @param nextLevel Estado del siguiente nivel.
+     */
     @Override
     public void init(int nextLevel) {
         super.init(nextLevel);
 
+        // Fondo
         temple = new Background("/Backgrounds/temple.gif", 0.5, 0);
+
+        // Mapa
         generateTileMap("/Maps/level4.map", 140, 0, false);
 
+        // Configuración del nivel
         setupGameObjects(80, 131, 2850, 120, false);
         setupTitle(new int[]{0, 0, 178, 20}, new int[]{0, 33, 91, 13});
         setupMusic("level4", "/Music/level1boss.mp3", true);
 
+        // Enemigos
         enemyTypesInLevel = new EnemyType[]{EnemyType.SPIRIT};
         coords = new int[][]{{150, 100}};
-
         populateEnemies(enemyTypesInLevel, coords);
     }
 
+    /**
+     * Actualiza el estado del nivel.
+     */
     @Override
     public void update() {
         super.update();
 
+        // Inicia el evento de terremoto si el jugador cruza el umbral
         if (player.getx() > 100 && !tileMap.isShaking()) {
             eventQuake = blockInput = true;
-            eventCount++;
         }
 
-        if (eventQuake) eventQuake();
+        if (eventQuake) {
+            eventQuake();
+        }
     }
 
+    /**
+     * Gestiona el evento de terremoto.
+     */
     private void eventQuake() {
         eventCount++;
-        if (eventCount == 1) {
-            player.stop();
-            player.setPosition(120, player.gety());
-            setPlayerEmote(Player.CONFUSED_EMOTE, 0);
-            tileMap.setShaking(true, 10);
-        } else if (eventCount == 60) {
-            setPlayerEmote(Player.CONFUSED_EMOTE, 0);
-        } else if (eventCount == 120) {
-            setPlayerEmote(Player.NONE_EMOTE, 0);
-        } else if (eventCount == 150) {
-            tileMap.setShaking(true, 10);
-        } else if (eventCount == 180) {
-            setPlayerEmote(Player.SURPRISED_EMOTE, 0);
-        } else if (eventCount == 300) {
-            setPlayerEmote(Player.NONE_EMOTE, 0);
-            eventQuake = blockInput = false;
-            eventCount = 0;
+        switch (eventCount) {
+            case 1:
+                player.stop();
+                player.setPosition(120, player.gety());
+                setPlayerEmote(Player.CONFUSED_EMOTE, 0);
+                tileMap.setShaking(true, 10);
+                break;
+            case 60:
+                setPlayerEmote(Player.CONFUSED_EMOTE, 0);
+                break;
+            case 120:
+                setPlayerEmote(Player.NONE_EMOTE, 0);
+                break;
+            case 150:
+                tileMap.setShaking(true, 10);
+                break;
+            case 180:
+                setPlayerEmote(Player.SURPRISED_EMOTE, 0);
+                break;
+            case 300:
+                setPlayerEmote(Player.NONE_EMOTE, 0);
+                eventQuake = blockInput = false;
+                eventCount = 0;
+                break;
+            default:
+                break;
         }
     }
 
+    /**
+     * Configura el emote del jugador y ajusta el contador del evento si es necesario.
+     *
+     * @param emote Emote a asignar al jugador.
+     * @param delay Tiempo adicional para el evento.
+     */
     private void setPlayerEmote(int emote, int delay) {
         player.setEmote(emote);
         if (delay > 0) {
--- src/main/java/al/tonikolaba/gamestate/OptionsState.java
+++ src/main/java/al/tonikolaba/gamestate/OptionsState.java
@@ -1,6 +1,6 @@
 /** Copyright to N.Kolaba
-All rights reserved ©.
-**/
+ All rights reserved ©.
+ **/
 
 package al.tonikolaba.gamestate;
 
@@ -12,49 +12,70 @@ import al.tonikolaba.entity.Player;
 import al.tonikolaba.handlers.Keys;
 
 /**
+ * Clase que representa el estado de opciones del menú.
+ * Permite al jugador interactuar con submenús como "HowTo Play" y "Language".
+ *
  * @author N.Kolaba
  */
 public class OptionsState extends BasicState {
 
+    private static final String HOW_TO_PLAY = "HowTo Play";
+    private static final String LANGUAGE = "Language";
+    private static final String BACK = "Back";
+
     private Player player; // Referencia al jugador
 
+    /**
+     * Constructor del estado de opciones.
+     *
+     * @param gsm    Gestor de estados del juego.
+     * @param player Referencia al jugador.
+     */
     public OptionsState(GameStateManager gsm, Player player) {
         super(gsm);
         this.player = player; // Asignar el jugador
-        options = new String[] { "HowTo Play", "Language", "Back" };
+        options = new String[] { HOW_TO_PLAY, LANGUAGE, BACK };
     }
 
+    /**
+     * Actualiza el estado del menú comprobando la entrada del jugador.
+     */
     @Override
     public void update() {
-        // check keys
         handleInput();
     }
 
+    /**
+     * Dibuja las opciones del menú en la pantalla.
+     *
+     * @param g Contexto gráfico para renderizar.
+     */
     @Override
-    public void draw(Graphics2D i) {
-        super.draw(i);
-        i.setFont(fontMenu);
+    public void draw(Graphics2D g) {
+        super.draw(g);
+        g.setFont(fontMenu);
 
-        i.setColor(Color.RED);
-        i.drawString("HowTo Play", 300, 223);
-        i.drawString("Language", 300, 248);
-        i.drawString("Back", 300, 273);
+        g.setColor(Color.RED);
+        g.drawString(HOW_TO_PLAY, 300, 223);
+        g.drawString(LANGUAGE, 300, 248);
+        g.drawString(BACK, 300, 273);
     }
 
+    /**
+     * Maneja la selección de opciones del menú.
+     */
     @Override
     protected void select() {
+        JukeBox.play("menuselect");
         switch (currentChoice) {
             case 0:
-                JukeBox.play("menuselect");
-                gsm.setState(GameStateManager.HOWTOPLAY); // Pasar el jugador
+                gsm.setState(GameStateManager.HOWTOPLAY); // Pasar al estado "HowTo Play"
                 break;
             case 1:
-                JukeBox.play("menuselect");
-                gsm.setState(GameStateManager.OPTIONSSTATE); // Pasar el jugador si hay subopciones
+                gsm.setState(GameStateManager.OPTIONSSTATE); // Ir al submenú de opciones
                 break;
             case 2:
-                JukeBox.play("menuselect");
-                gsm.setState(GameStateManager.MENUSTATE); // Regresar al menú principal con el jugador
+                gsm.setState(GameStateManager.MENUSTATE); // Regresar al menú principal
                 break;
             default:
                 gsm.setState(GameStateManager.MENUSTATE);
@@ -62,17 +83,27 @@ public class OptionsState extends BasicState {
         }
     }
 
+    /**
+     * Controla las entradas del jugador para navegar por el menú.
+     */
     @Override
     public void handleInput() {
-        if (Keys.isPressed(Keys.ENTER))
+        if (Keys.isPressed(Keys.ENTER)) {
             select();
-        if (Keys.isPressed(Keys.UP) && currentChoice > 0) {
-            JukeBox.play("menuoption", 0);
-            currentChoice--;
+        } else if (Keys.isPressed(Keys.UP) && currentChoice > 0) {
+            navigateMenu(-1);
+        } else if (Keys.isPressed(Keys.DOWN) && currentChoice < options.length - 1) {
+            navigateMenu(1);
         }
-        if (Keys.isPressed(Keys.DOWN) && currentChoice < options.length - 1) {
-            JukeBox.play("menuoption", 0);
-            currentChoice++;
-        }
+    }
+
+    /**
+     * Cambia la opción seleccionada en el menú.
+     *
+     * @param direction Dirección de la navegación (-1 para arriba, 1 para abajo).
+     */
+    private void navigateMenu(int direction) {
+        JukeBox.play("menuoption", 0);
+        currentChoice += direction;
     }
 }
