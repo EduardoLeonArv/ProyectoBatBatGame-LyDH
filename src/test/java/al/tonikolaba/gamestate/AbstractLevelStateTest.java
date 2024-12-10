e762d0aa9c0302e05fd7919035290252b8962f22
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-12-10 20:58:54 +0000

    Update AbstractLevelStateTest.java

--- src/test/java/al/tonikolaba/gamestate/AbstractLevelStateTest.java
+++ src/test/java/al/tonikolaba/gamestate/AbstractLevelStateTest.java
@@ -7,44 +7,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import al.tonikolaba.entity.Player;
-import al.tonikolaba.entity.enemies.Flyer.FlyerType;
 import al.tonikolaba.entity.Enemy.EnemyType;
 import al.tonikolaba.tilemap.Background;
 
 public class AbstractLevelStateTest {
 
     @Test
-    @DisplayName("Test AbstractLevelState Initialization")
-    public void testInit() {
+    @DisplayName("Test LevelTest Initialization")
+    public void testLevelTestInit() {
         // Mock dependencies
         GameStateManager gsmMock = mock(GameStateManager.class);
         Player playerMock = mock(Player.class);
 
-        // Create anonymous subclass to test abstract methods
-        AbstractLevelState levelState = new AbstractLevelState(gsmMock, playerMock) {
-            @Override
-            protected int[][] getEnemyCoordinates() {
-                return new int[][]{{100, 200}, {300, 400}};
-            }
-
-            @Override
-            protected EnemyType[] getEnemyTypes() {
-                return new EnemyType[]{EnemyType.RED_ENERGY, EnemyType.UFO};
-            }
-
-            @Override
-            protected String getMapPath() {
-                return "/maps/testmap.tmx";
-            }
-
-            @Override
-            protected String getMusicPath() {
-                return "/music/testmusic.mp3";
-            }
-        };
+        // Create instance of LevelTest
+        LevelTest levelTest = new LevelTest(gsmMock, playerMock);
 
         // Call init and ensure no exceptions occur
-        levelState.init(1);
-        assertNotNull("Background should be initialized", levelState.temple);
+        levelTest.init(1);
+        assertNotNull("Background should be initialized", levelTest.temple);
     }
 }
