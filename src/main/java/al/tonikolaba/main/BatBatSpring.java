7a2f54d9edbf95b8c01ade626061f7d5f4874a32
Eduardo Leon Arvelo <alu0101475615@ull.edu.es>
2024-12-19 21:57:41 +0000

    Update BatBatSpring.java

--- src/main/java/al/tonikolaba/main/BatBatSpring.java
+++ src/main/java/al/tonikolaba/main/BatBatSpring.java
@@ -36,9 +36,18 @@ public class BatBatSpring {
 	 */
 	@Bean
 	public DocumentBuilderFactory documentBuilderFactory() {
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		factory.setAttribute(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-		return factory;
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+			factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+			factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, ""); // Bloquea acceso a DTDs externos
+			factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, ""); // Bloquea acceso a esquemas externos
+			return factory;
+		} catch (Exception e) {
+			throw new IllegalStateException("Error al configurar DocumentBuilderFactory de forma segura", e);
+		}
 	}
 	@Bean
 	public ObjectMapper objectMapper() {
